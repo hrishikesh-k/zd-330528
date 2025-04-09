e273b20dfd0861fd8865e97332baf8b5fe574760
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-04-09 22:18:10 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,10 @@ export const config = {
   matcher: '/((?!csp|_next|static|favicon).*)',
 };
 
-const middleware = (): Promise<NextResponse> => {
+const middleware = (): NextResponse => {
   console.info('Next Middleware was called');
 
-  return Promise.resolve(NextResponse.next());
+  return NextResponse.next()
 };
 
 export default middleware;
